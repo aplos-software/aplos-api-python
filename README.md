a51c100ba91e2e01a06edcf93fcea4c174236d7d
Derek Scharton <derek@aplos.com>
2019-05-11 21:23:50 -0700

    Start with basic example for API

--- README.md
+++ README.md
@@ -1,1 +1,26 @@
+# About
+
 This is the home of Aplos API samples written in Python.
+
+`api_main.py` is a basic example of the flow to request a token
+and decrypt it with your private key followed by an API request.
+
+## Private Key
+
+Save your downloaded private key as `private.key` in the project root and surround the key data 
+with the following:
+
+```
+-----BEGIN PRIVATE KEY-----
+key data
+-----END PRIVATE KEY-----
+```
+
+## key.cfg
+
+Create `key.cfg` from the template file and replace `key` with your Aplos client ID.
+
+## References
+
+* [Requests HTTP Library](https://2.python-requests.org/en/master/)
+* [Cryptography Library](https://cryptography.io/en/latest/)
---
+++ api_main.py
@@ -0,0 +1,51 @@
+#!/usr/bin/python3
+import requests
+from cryptography.hazmat.backends import default_backend
+from cryptography.hazmat.primitives import serialization
+from cryptography.hazmat.backends import default_backend
+from cryptography.hazmat.primitives.asymmetric import rsa
+from cryptography.hazmat.primitives.asymmetric import padding
+from cryptography.hazmat.primitives import hashes
+import configparser as cp
+import base64
+
+config_file = 'key.cfg'
+key_file = 'private.key'
+aplos_domain = "www.aplos.com"
+auth_route = "hermes/api/v1/auth"
+accounts_route = "hermes/api/v1/accounts"
+payables_route = "hermes/api/v1/payables"
+
+
+if __name__ == "__main__":
+    config = cp.ConfigParser()
+    config.read(config_file)
+    key = config['Aplos']['key']
+    with open(key_file, 'rb') as k:
+        api_request = '{}://{}/{}/{}'.format('https', aplos_domain, auth_route, key)
+        print("request: {}".format(api_request))
+
+        r = requests.get(api_request)
+        if r.status_code == 200:
+            token = r.json()['data']['token']
+            print("response: {}".format(r.json()))
+            print("token: {}".format(token))
+
+        private_key = serialization.load_pem_private_key(
+            k.read(),
+            password=None,
+            backend=default_backend()
+        )
+
+    token64 = base64.b64decode(token)
+    decryptToken = private_key.decrypt(token64, padding.PKCS1v15())
+    decryptTokenUtf8 = str(decryptToken, 'utf-8')
+    print("decrypted token: {}".format(decryptTokenUtf8))
+        
+    accounts = '{}://{}/{}'.format('https', aplos_domain, payables_route)
+    h = {'Authorization':'Bearer {}'.format(decryptTokenUtf8)}
+
+    # JSON format response
+    response = requests.get(accounts, headers=h, params={"page_size": 2, "page_num": 1})
+    print('Status code: {}'.format(response.status_code))
+    print('Response data:\n{}'.format(response.text))
